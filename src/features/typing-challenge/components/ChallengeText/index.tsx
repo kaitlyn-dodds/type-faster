8c017ce39dec45b57580b826d3850bc493a29893
kaitlyn.dodds <sub@kadodds.com>
2025-12-19 20:36:43 -0700

    fixing issue w/ words display, also viewport width being way too wide

--- src/features/typing-challenge/components/ChallengeText/index.tsx
+++ src/features/typing-challenge/components/ChallengeText/index.tsx
@@ -11,27 +11,51 @@ interface ChallengeProps {
 function ChallengeText({ challengeTokens, submittedTokens, cursor }: ChallengeProps) {
     return (
         <div className="challenge-text">
-            {challengeTokens.map((token, index) => {
-                let status: 'default' | 'entered' | 'enteredIncorrect' = 'default'
-                const submittedToken = submittedTokens[index]
+            {
+                (() => {
+                    let globalIndex = 0
+                    const words: ChallengeToken[][] = []
+                    let currentWord: ChallengeToken[] = []
 
-                if (submittedToken) {
-                    if (submittedToken.value === token.value) {
-                        status = 'entered'
-                    } else {
-                        status = 'enteredIncorrect'
+                    challengeTokens.forEach((token) => {
+                        currentWord.push(token)
+                        if (token.value === " ") {
+                            words.push(currentWord)
+                            currentWord = []
+                        }
+                    })
+                    if (currentWord.length > 0) {
+                        words.push(currentWord)
                     }
-                }
 
-                return (
-                    <ChallengeTokenDisplay
-                        key={index}
-                        display={token.value}
-                        status={status}
-                        isCursor={index === cursor}
-                    />
-                )
-            })}
+                    return words.map((word, wordIndex) => (
+                        <div key={wordIndex} className="challenge-word">
+                            {word.map((token) => {
+                                const index = globalIndex++
+                                let status: 'default' | 'entered' | 'enteredIncorrect' = 'default'
+                                const submittedToken = submittedTokens[index]
+
+                                if (submittedToken) {
+                                    if (submittedToken.value === token.value) {
+                                        status = 'entered'
+                                    } else {
+                                        status = 'enteredIncorrect'
+                                    }
+                                }
+
+                                return (
+                                    <ChallengeTokenDisplay
+                                        key={index}
+                                        display={token.value}
+                                        status={status}
+                                        isCursor={index === cursor}
+                                    />
+                                )
+                            })}
+                        </div>
+                    ))
+                })()
+            }
         </div>
     )
 }
